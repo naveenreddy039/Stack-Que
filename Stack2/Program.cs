bb330b21f90384d3635323a48fc65d67a4f491c8
naveen <naveenreddy.k07@gmail.com>
2021-11-20 18:01:55 +0530

    fitst commit

---
+++ Stack2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push("70");
+            stack.Push("30");
+            stack.Push("56");
+            foreach (string item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+            stack.Pop();
+
+            foreach (string item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
