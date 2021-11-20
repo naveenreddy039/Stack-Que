c72f9367ce78013586818755985b16a5c3d4a834
naveen <naveenreddy.k07@gmail.com>
2021-11-20 18:42:41 +0530

    fitst commit

---
+++ Queue2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("56");
+            queue.Enqueue("30");
+            queue.Enqueue("70");
+
+            foreach (string item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------");
+            queue.Dequeue();
+
+            foreach (string item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine(queue.Peek());
+        }
+    }
+}
